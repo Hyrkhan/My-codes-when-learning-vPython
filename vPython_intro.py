85d71a6e1fe361623195efa6ba2861e44a01efa1
Hyrkan <109449003+Hyrkhan@users.noreply.github.com>
2022-10-02 16:10:05 +0800

    Add files via upload

---
+++ vPython_intro.py
@@ -0,0 +1,15 @@
+#very first try on vpython
+#this functions create shapes on a chrome window tab and you can customize its size and color
+from vpython import *
+from time import * 
+#ball = sphere(color = color.red)
+#sleep(5)
+#ball.color = color.blue
+#sleep(5)
+#ball.color = color.green
+#myBox = box(color = color.magenta,length = 12, width = 1, height = .5)
+#myTube = cylinder(color = color.white,length = 1,radius = 1)
+floor = box(pos = vector(0,-5,0),color = color.white, length = 10, height = .1, width = 10)
+ceiling = box(pos = vector(0,5,0),color = color.white, length = 10, height = .1, width = 10)
+while True: #while loop is important to constantly show it
+    pass
